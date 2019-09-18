148e9310fabe6f588d24603d744016e210607011
LIAN0856 <924738370@qq.com>
2019-09-18 19:10:46 +0800

    interesting

---
+++ jungle.java
@@ -0,0 +1,13 @@
+package homework2;
+
+public class jungle {
+	public static void main(String[]args) {
+		int x=1;
+		for(int y=0;y<5;y++)//被恶龙打了五巴掌的五次喝神仙水循环
+		{	
+			x=x+1;
+			x=x/2;
+		}
+		System.out.println(x);
+	}
+}
